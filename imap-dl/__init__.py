c906a75d1a06cf25ac5b3aaf0cf015ace49e00d2
Zahiruzzaman Chowdhury <nebrus.gamer@gmail.com>
2024-06-06 00:04:21 +0600

    Add exception handling

--- imap-dl/__init__.py
+++ imap-dl/__init__.py
@@ -1,6 +1,8 @@
 import imaplib
 import json
 from os import chdir
+from socket import gaierror
+from sys import exit
 
 # Retrieve host and credentials from config
 chdir(__file__.removesuffix("/__init__.py"))
@@ -10,23 +12,33 @@ with open("config.json") as file:
     USER = config["email"]
     PASS = config["password"]
 
-# Login to mailbox
-mailbox = imaplib.IMAP4_SSL(HOST)
-mailbox.login(USER, PASS)
+try:
+    # Login to mailbox
+    mailbox = imaplib.IMAP4_SSL(HOST)
+    mailbox.login(USER, PASS)
 
-# Retrieve all mail from inbox
-mailbox.select("Inbox")
-typ, data = mailbox.search(None, "ALL")
+    # Retrieve all mail from inbox
+    mailbox.select("Inbox")
+    typ, data = mailbox.search(None, "ALL")
 
-# Retrieve individual mail
-count = 1
-for num in data[0].split():
-    typ, data = mailbox.fetch(num, "(RFC822)")
-    with open("mail.eml", "wb") as file:
-        file.write(data[0][1])
-    print("Mail %i received" % count)
-    count += 1
+    # Retrieve individual mail
+    count = 1
+    for num in data[0].split():
+        typ, data = mailbox.fetch(num, "(RFC822)")
+        with open("mail.eml", "wb") as file:
+            file.write(data[0][1])
+        print("Mail %i received" % count)
+        count += 1
 
-# End connection to IMAP server
-mailbox.close()
-mailbox.logout()
+# Handle net error or bad hostname
+except gaierror or NameError as e:
+    print("E: Connection could not be made - probably because of no internet or bad config")
+
+# Handle incorrect authentication
+except imaplib.IMAP4.error as e:
+    print("E: Authentication error - ")
+
+# End connection to IMAP server if no error occurred
+else:
+    mailbox.close()
+    mailbox.logout()
