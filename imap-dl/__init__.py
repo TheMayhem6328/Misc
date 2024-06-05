82a242b81c5579b0dac2dc6704441089cbc0c2cb
Zahiruzzaman Chowdhury <nebrus.gamer@gmail.com>
2024-06-06 02:25:37 +0600

    Implement bulk saving

--- imap-dl/__init__.py
+++ imap-dl/__init__.py
@@ -1,11 +1,17 @@
 import imaplib
 import json
-from os import chdir
+import email
+from os import chdir, mkdir
+from os.path import exists
 from socket import gaierror
-from sys import exit
 
-# Retrieve host and credentials from config
+# Resolve directories and create as needed
+DIRNAME = "emails"
 chdir(__file__.removesuffix("/__init__.py"))
+if not exists(DIRNAME):
+    mkdir(DIRNAME)
+
+# Retrieve host and credentials from config.json
 with open("config.json") as file:
     config: dict = json.load(file)
     HOST = config["host"]
@@ -22,23 +28,30 @@ try:
     typ, data = mailbox.search(None, "ALL")
 
     # Retrieve individual mail
-    count = 1
+    chdir(DIRNAME)
     for num in data[0].split():
         typ, data = mailbox.fetch(num, "(RFC822)")
-        with open("mail.eml", "wb") as file:
+        mail = email.message_from_bytes(data[0][1])
+        date = email.utils.parsedate_to_datetime(mail["Date"]).isoformat()
+        subject = mail["Subject"]
+        filename = f"{str(num, encoding='ASCII').zfill(5)} ---- {date} ---- {subject}.eml"
+        with open(filename, "wb") as file:
             file.write(data[0][1])
-        print("Mail %i received" % count)
-        count += 1
+        print("I: Saved - %s" % filename)
 
 # Handle net error or bad hostname
 except gaierror or NameError as e:
-    print("E: Connection could not be made - probably because of no internet or bad config")
+    print("E: Connection could not be made - check internet and host information in config.json")
 
 # Handle incorrect authentication
 except imaplib.IMAP4.error as e:
-    print("E: Authentication error - ")
+    print("E: Authentication error - check login details in config.json")
 
-# End connection to IMAP server if no error occurred
+# End connection to IMAP server if no errors occurred or terminated using Ctrl+C
+except KeyboardInterrupt:
+    print("W: Keyboard interrupt detected - exiting")
+    mailbox.close()
+    mailbox.logout()
 else:
     mailbox.close()
     mailbox.logout()
