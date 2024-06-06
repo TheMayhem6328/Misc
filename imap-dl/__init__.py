5bbc59db8ccedf8f0a67a4b2e8b1c18b3e95fcf2
Zahiruzzaman Chowdhury <nebrus.gamer@gmail.com>
2024-06-06 10:52:59 +0600

    Refactored and optimized

--- imap-dl/__init__.py
+++ imap-dl/__init__.py
@@ -4,6 +4,7 @@ import email
 from os import chdir, mkdir
 from os.path import exists
 from socket import gaierror
+from sys import exit
 
 # Resolve directories and create as needed
 DIRNAME = "emails"
@@ -18,40 +19,67 @@ with open("config.json") as file:
     USER = config["email"]
     PASS = config["password"]
 
-try:
-    # Login to mailbox
-    mailbox = imaplib.IMAP4_SSL(HOST)
-    mailbox.login(USER, PASS)
+# Login to mailbox
+mailbox = imaplib.IMAP4_SSL(HOST)
+mailbox.login(USER, PASS)
 
-    # Retrieve all mail from inbox
-    mailbox.select("Inbox")
-    typ, data = mailbox.search(None, "ALL")
+# Retrieve all mail from inbox
+mailbox.select("Inbox")
+_, data = mailbox.search(None, "ALL")
 
-    # Retrieve individual mail
-    chdir(DIRNAME)
-    for num in data[0].split():
-        typ, data = mailbox.fetch(num, "(RFC822)")
-        mail = email.message_from_bytes(data[0][1])
-        date = email.utils.parsedate_to_datetime(mail["Date"]).isoformat()
-        subject = mail["Subject"]
-        filename = f"{str(num, encoding='ASCII').zfill(5)} ---- {date} ---- {subject}.eml"
+
+# Function for mail retrieval
+def retrieve_mail(mailbox: imaplib.IMAP4, upper: int, lower:int):
+    print(upper, lower)
+    _, data = mailbox.fetch(f"{lower}:{upper}", "(RFC822)")
+    for mail in data:
+        # Skip if item is not proper mail
+        if not isinstance(mail, tuple):
+            continue
+
+        # Parse mail
+        mail_parsed = email.message_from_bytes(mail[1])
+        date = email.utils.parsedate_to_datetime(mail_parsed["Date"]).isoformat()
+        subject = "".join(char for char in mail_parsed["Subject"] if char not in "\/:*?<>|")[0:50]
+
+        # Save to file
+        filename = f"{date} ---- {subject}.eml"
         with open(filename, "wb") as file:
-            file.write(data[0][1])
+            file.write(mail[1])
         print("I: Saved - %s" % filename)
 
-# Handle net error or bad hostname
-except gaierror or NameError as e:
-    print("E: Connection could not be made - check internet and host information in config.json")
 
-# Handle incorrect authentication
-except imaplib.IMAP4.error as e:
-    print("E: Authentication error - check login details in config.json")
+if __name__ == "__main__":
+    try:
+        # Retrieve individual mail
+        chdir(DIRNAME)
+        mail_count = len(data[0].split())
+        while True:
+            if mail_count > 100:
+                retrieve_mail(mailbox, mail_count, mail_count := mail_count - 100)
+                mail_count -= 1
+            else:
+                retrieve_mail(mailbox, mail_count, 1)
+                break
+            
 
-# End connection to IMAP server if no errors occurred or terminated using Ctrl+C
-except KeyboardInterrupt:
-    print("W: Keyboard interrupt detected - exiting")
-    mailbox.close()
-    mailbox.logout()
-else:
-    mailbox.close()
-    mailbox.logout()
+    # Handle net error or bad hostname
+    except gaierror or NameError as e:
+        print(
+            "E: Connection could not be made - check internet and host information in config.json"
+        )
+
+    # Handle incorrect authentication
+    except imaplib.IMAP4.error as e:
+        print("E: Authentication error - check login details in config.json")
+
+    # End connection to IMAP server if no errors occurred or terminated using Ctrl+C
+    except KeyboardInterrupt:
+        print("\nW: Keyboard interrupt detected - exiting")
+        mailbox.close()
+        mailbox.logout()
+        exit()
+    else:
+        mailbox.close()
+        mailbox.logout()
+        exit()
